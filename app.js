2ea892f4f9c64cd7ec9cc5dc38e09d58cb90ac6d
profesor2199 <abdoloux@gmail.com>
2021-05-02 12:23:12 +0000

    FEATURES added

--- app.js
+++ app.js
@@ -8,18 +8,36 @@ hamburger.addEventListener('click', () => {
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+
+/*loop hero h1*/
+  var text1 = ["Diary & Journal","Write your own", "Personal notes on"];
+  var text2 = ["- private writing","Private diary!",  "Web and Android!"];
+  var counter1 = 0;
+  var counter2 = 0;
+  var inst = setInterval(change, 5000);
+  function change(){
+  document.getElementById("looph1").innerHTML = text1[counter1];
+  document.getElementById("looph2").innerHTML = text2[counter2];
+  counter1++;
+  counter2++;
+
+   if (counter1 >= text1.length) {
+      counter1 = 0;
+      // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+    }
+    if (counter2 >= text2.length) {
+      counter2 = 0;
+      // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+    }
+  }
+  /*End loop hero h1*/
+
+
